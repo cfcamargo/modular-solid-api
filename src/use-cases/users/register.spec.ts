2bdb86501fbf755d0d66712e599f6a442c0dbaa8
Cristian Camargo <contato@camargodev.com.br>
2024-09-12 00:04:46 -0400

    test:create in memory database and test register user

--- src/use-cases/users/register.spec.ts
+++ src/use-cases/users/register.spec.ts
@@ -2,10 +2,12 @@ import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-reposi
 import { RegisterUserUseCase } from './register-use-case'
 import { describe, expect, it } from 'vitest'
 import { compare } from 'bcryptjs'
+import { UseerAlreadyExistsError } from '../errors/user-already-exists-error'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repositoru'
 
 describe('Register use case', () => {
     it('should hash user password upon registration', async() => {
-        const prismaUsersRepository = new PrismaUsersRepository()
+        const prismaUsersRepository = new InMemoryUsersRepository()
         const registerUseCase = new RegisterUserUseCase(prismaUsersRepository)
 
         const { user } = await registerUseCase.handle({
@@ -17,5 +19,41 @@ describe('Register use case', () => {
         const isPasswordCorrectlyHashed = await compare('123456', user.hashed_password)
 
         expect(isPasswordCorrectlyHashed).toBe(true)
+    })
+
+    it('it should not be able to register with same email twice', async() => {
+        const prismaUsersRepository = new InMemoryUsersRepository()
+        const registerUseCase = new RegisterUserUseCase(prismaUsersRepository)
+
+        const email = 'jhondoe@example.com'
+
+        await registerUseCase.handle({
+            name: 'Jhon Doe',
+            email,
+            password: '123456'
+        })
+
+        await expect(() => 
+            registerUseCase.handle({
+                name: 'Jhon Doe',
+                email,
+                password: '123456'
+            })
+        ).rejects.toBeInstanceOf(UseerAlreadyExistsError)
+    })
+
+    it('it should be able to register', async() => {
+        const prismaUsersRepository = new InMemoryUsersRepository()
+        const registerUseCase = new RegisterUserUseCase(prismaUsersRepository)
+
+        const email = 'jhondoe@example.com'
+
+        const { user } = await registerUseCase.handle({
+            name: 'Jhon Doe',
+            email,
+            password: '123456'
+        })
+
+        expect(user.id).toEqual(expect.any(String))
     })
 })
